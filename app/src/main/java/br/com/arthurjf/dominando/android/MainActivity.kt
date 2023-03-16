e33c8dd123a2da3a1ea0e8c26a4e066683f39c4c
arthurjf <arthur.310100@hotmail.com>
2023-03-16 11:14:56 -0300

    Teste com arquivos de recurso

--- app/src/main/java/br/com/arthurjf/dominando/android/MainActivity.kt
+++ app/src/main/java/br/com/arthurjf/dominando/android/MainActivity.kt
@@ -1,6 +1,6 @@
 package br.com.arthurjf.dominando.android
 
-import androidx.appcompat.app.AppCompatActivity
+import  androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
